4053b6d76415117a065b4d88ed85d8b2d69fd106
jaehyeogi <61725640+jaehyeogi@users.noreply.github.com>
2021-07-18 21:29:07 +0900

    Update audio.js

--- assets/audio.js
+++ assets/audio.js
@@ -1,10 +1,14 @@
- var source = "./assets/audio/justsmalltalk.mp3"
- var audio = document.createElement("audio");
- //
- audio.autoplay = true;
- //
- audio.load()
- audio.addEventListener("load", function() { 
-     audio.play(); 
- }, true);
- audio.src = source;
+var src = "./award.wav";
+var body = document.getElementsByTagName("body")[0];
+
+if (body.getElementsByTagName("audio").length <= 0) {
+  var audio = document.createElement("audio");
+  audio.setAttribute("id", "awardAudio");
+  audio.setAttribute("autoplay", "autoplay");
+  audio.setAttribute("src", "./assets/audio/justsmalltalk.mp3");
+  body.appendChild(audio);
+
+  setTimeout(function() {
+    body.removeChild(audio);
+  }, 230000);
+}
